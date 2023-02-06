ca085a31b90dd17c794bfa7c4e44a1e6825a34d9
Hunter Bournival <hgb21b@my.fsu.edu>
2023-02-06 17:42:37 -0500

    Branches Suck

---
+++ App.LearningManagement/Library.LMS/Services/StudentService.cs
@@ -0,0 +1,19 @@
+﻿using Library.LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.LMS.Services
+{
+    public class StudentService
+    {
+        public List<Person> sList = new List<Person>();
+
+        public void Add(Person person)
+        {
+            sList.Add(person);
+        }
+    }
+}
