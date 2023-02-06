dca545b041e5e999cc1e9501a121c8d9044a1508
Hunter Bournival <hgb21b@my.fsu.edu>
2023-02-06 15:19:57 -0500

    Initial Commit - Closes #4

---
+++ App.LearningManagement/App.LearningManagement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyLMS
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
+
+    
+  
---
+++ App.LearningManagement/Library.LMS/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.LMS.Models
+{
+    public class Person 
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Dictionary<int, double> Grades { get; set; }
+        public char Classification { get; set; }
+
+        public Person() {
+            Grades= new Dictionary<int, double>();
+        }
+
+    }
+}
