5b22cfd3d1db407c6a76173147914ee324788b22
Hunter Bournival <hgb21b@my.fsu.edu>
2023-03-17 14:15:42 -0400

    Cleaned Up Users main Loop - Closes #53

--- App.LearningManagement/App.LearningManagement/Program.cs
+++ App.LearningManagement/App.LearningManagement/Program.cs
@@ -22,75 +22,118 @@ namespace MyLMS
             
             while(cont)
             {
-
                 Console.WriteLine("\nChoose an Action:");
                 Console.WriteLine("0. Exit");
-                Console.WriteLine("1. Add a Student Enrollment");
-                Console.WriteLine("2. List All Enrolled Studnets");
-                Console.WriteLine("3. Search for a Student");
-                Console.WriteLine("4. Add a New Course");
-                Console.WriteLine("5. Update a Student Enrollment");
-                Console.WriteLine("6. List All Courses");
-                Console.WriteLine("7. Update a Course");
-                Console.WriteLine("8. Search for a Course");
-                Console.WriteLine("9. Create an Assignment");
-                Console.WriteLine("10. Search for Students Courses");
-                Console.WriteLine("11. List All Assignments");
+                Console.WriteLine("1. Maintain Students");
+                Console.WriteLine("2. Maintain Courses");
+
+                
+                
+                
+                
+                /*Console.WriteLine("9. Create an Assignment");
+                
+                Console.WriteLine("11. List All Assignments");*/
                 var input = Console.ReadLine();
 
                 if (int.TryParse(input, out int result))
                 {
                     if (result == 0) { cont = false; }
 
-                    else if (result == 1)
+                    else if(result== 1) 
                     {
-                        studentHelper.CreateStudentRecord();
+                        ShowStudentMenu(studentHelper);
                     }
 
                     else if(result == 2)
                     {
-                        studentHelper.ListStudents(1);
+                        ShowCourseMenu(courseHelper);
                     }
-
-                    else if(result == 3)
-                    {
-                        studentHelper.SearchStudents();
-                    }
-                    else if(result == 4)
-                    {
-                        courseHelper.CreateCourseRecord();
-                    }
-                    else if(result == 5)
-                    {
-                        studentHelper.UpdateStudentRecord();
-                    }
-                    else if(result == 6)
-                    {
-                        courseHelper.ListCourses();
-                    }
-                    else if(result == 7)
-                    {
-                        courseHelper.UpdateCourseRecord();
-                    }
-                    else if(result == 8)
-                    {
-                        courseHelper.SearchCourses();
-                    }
-                    else if(result == 9)
+                    
+                    /*else if(result == 9)
                     {
                         assignmentHelper.CreateAssignment();
                     }
-                    else if(result == 10)
-                    {
-                        studentHelper.ListStudents(0);
-                    }
+                    
                     else if(result == 11)
                     {
                         assignmentHelper.ListAssignments();
-                    }
+                    }*/
                 }
             }
             
+        }
+
+        static void ShowStudentMenu(StudentHelper studentHelper)
+        {
+            Console.WriteLine("\nChoose an Action:");
+            Console.WriteLine("1. Add a Student Enrollment");
+            Console.WriteLine("2. List All Enrolled Studnets");
+            Console.WriteLine("3. Search for a Student");
+            Console.WriteLine("4. Update a Student Enrollment");
+            Console.WriteLine("5. Search for Students Courses");
+
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out int result))
+            {
+
+
+                if (result == 1)
+                {
+                    studentHelper.CreateStudentRecord();
+                }
+
+                else if (result == 2)
+                {
+                    studentHelper.ListStudents(1);
+                }
+
+                else if (result == 3)
+                {
+                    studentHelper.SearchStudents();
+                }
+                else if (result == 4)
+                {
+                    studentHelper.UpdateStudentRecord();
+                }
+                else if (result == 5)
+                {
+                    studentHelper.ListStudents(0);
+                }
+            }
+
+
+        }
+
+        static void ShowCourseMenu(CourseHelper courseHelper)
+        {
+            Console.WriteLine("1. Add a New Course");
+            Console.WriteLine("2. List All Courses");
+            Console.WriteLine("3. Update a Course");
+            Console.WriteLine("4. Search for a Course");
+
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out int result))
+            {
+                if (result == 1)
+                {
+                    courseHelper.CreateCourseRecord();
+                }
+
+                else if (result == 2)
+                {
+                    courseHelper.ListCourses();
+                }
+                else if (result == 3)
+                {
+                    courseHelper.UpdateCourseRecord();
+                }
+                else if (result == 4)
+                {
+                    courseHelper.SearchCourses();
+                }
+            }
+
         }
     }
 }
