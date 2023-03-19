29662c36dc87066b9c52d1e0df13c78f58fedd83
Hunter Bournival <hgb21b@my.fsu.edu>
2023-03-18 21:52:34 -0400

    Added Credit Hours to Course

--- App.LearningManagement/Library.LMS/Models/Course.cs
+++ App.LearningManagement/Library.LMS/Models/Course.cs
@@ -6,6 +6,7 @@ namespace Library.LMS.Models
         public string? Code { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+        public string? CreditHours { get; set; }
         public List<Person> Roster { get; set; }
         public List<Assignment> Assignments { get; set; }
         public List<Module> Modules { get; set; }
@@ -15,6 +16,7 @@ namespace Library.LMS.Models
             Code = string.Empty;
             Name = string.Empty;
             Description = string.Empty;
+            CreditHours = string.Empty;
             Roster = new List<Person>();
             Assignments = new List<Assignment>();
             Modules = new List<Module>();
